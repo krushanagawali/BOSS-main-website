9e84ca7ab2c581f9503c5377e95a6edcd8bf4b9d
krushanagawali <krushanagawali9@gmail.com>
2026-07-05 15:18:08 +0530

    Fix formatting in app.config.ts

--- boss-main-website/src/app/app.config.ts
+++ boss-main-website/src/app/app.config.ts
@@ -8,5 +8,5 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes,withHashLocation()), provideClientHydration(withEventReplay())
-  ]
+  ] 
 };
