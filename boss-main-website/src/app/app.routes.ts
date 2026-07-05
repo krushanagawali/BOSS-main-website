033248bafa00adf40ad51e1a08c2f76b54b40d85
krushanagawali <krushanagawali9@gmail.com>
2026-07-05 13:23:09 +0530

    Define routes for legal pages
    Added legal pages routes for privacy policy, terms and conditions, cookies policy, and disclaimer.

--- boss-main-website/src/app/app.routes.ts
+++ boss-main-website/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+  // 5. Legal Pages (Must come BEFORE the '**' route)
+  { 
+    path: 'privacy-policy', 
+    loadComponent: () => import('./legal/privacy-policy/privacy-policy').then(m => m.PrivacyPolicy) 
+  },
+  { 
+    path: 'terms-conditions', 
+    loadComponent: () => import('./legal/terms-conditions/terms-conditions').then(m => m.TermsConditions) 
+  },
+  { 
+    path: 'cookies-policy', 
+    loadComponent: () => import('./legal/cookies-policy/cookies-policy').then(m => m.CookiesPolicy) 
+  },
+  { 
+    path: 'disclaimer', 
+    loadComponent: () => import('./legal/disclaimer/disclaimer').then(m => m.Disclaimer) 
+  },
+
+];
